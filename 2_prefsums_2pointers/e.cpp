2f6552a96ce745c04d47f60e9026bdef11e699a3
Afanasyev Aleksander <kasar.looper@gmail.com>
2025-01-07 20:21:33 +0300

    Solved E from 2

--- 2_prefsums_2pointers/e.cpp
+++ 2_prefsums_2pointers/e.cpp
@@ -1,50 +1,65 @@
 #include <iostream>
 #include <vector>
 
+typedef long long ll;
 using namespace std;
 
 int main() {
-    int n, m, k, q;
-    cin >> n >> m >> k;
-    vector<vector<vector<long long>>> a(n, vector<vector<long long>>(m, vector<long long>(k, 0)));
-    for (int z = 0; z < n; z++)
-        for (int y = 0; y < m; y++)
-            for (int x = 0; x < k; x++)
-                cin >> a[z][y][x];
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    vector<vector<vector<long long>>> pref(n+1, vector<vector<long long>>(m+1, vector<long long>(k+1, 0)));
-    for (int z = 1; z <= n; z++)
-        for (int y = 1; y <= m; y++)
-            for (int x = 1; x <= k; x++)
-                pref[z][y][x] = a[z - 1][y - 1][x - 1]
+	int n, m, k;
+	cin >> n >> m >> k;
 
-                                - pref[z - 1][y][x]
-                                - pref[z][y - 1][x]
-                                - pref[z][y][x - 1]
+	vector<vector<vector<ll>>> a(n,
+		vector<vector<ll>>(m,
+			vector<ll>(k, 0)));
+	for (int z = 0; z < n; z++) {
+		for (int y = 0; y < m; y++) {
+			for (int x = 0; x < k; x++) {
+				cin >> a[z][y][x];
+			}
+		}
+	}
 
-                                + pref[z - 1][y - 1][x]
-                                + pref[z][y - 1][x - 1]
-                                + pref[z - 1][y][x - 1]
+	vector<vector<vector<ll>>> pref(n + 1,
+		vector<vector<ll>>(m + 1,
+			vector<ll>(k + 1, 0)));
+	for (int z = 0; z < n; z++) {
+		for (int y = 0; y < m; y++) {
+			for (int x = 0; x < k; x++) {
+				pref[z + 1][y + 1][x + 1] = a[z][y][x]
+					+ pref[z][y + 1][x + 1]
+					+ pref[z + 1][y][x + 1]
+					+ pref[z + 1][y + 1][x]
 
-                                - pref[z - 1][y - 1][x - 1];
+					- pref[z + 1][y][x]
+					- pref[z][y + 1][x]
+					- pref[z][y][x + 1]
 
-    cin >> q;
-    for (int i = 0; i < q; i++) {
-        int lx, ly, lz, rx, ry, rz;
-        cin >> lx >> ly >> lz >> rx >> ry >> rz;
+					+ pref[z][y][x];
+			}
+		}
+	}
 
-        long long ans = pref[rz][ry][rx]
+	int q;
+	cin >> q;
 
-                        + pref[lz - 1][ry][rx]
-                        + pref[rz][ly - 1][rx]
-                        + pref[rz][ry][lx - 1]
+	ll sum;
+	int lx, ly, lz, rx, ry, rz;
+	for (int _ = 0; _ < q; _++) {
+		cin >> lz >> ly >> lx >> rz >> ry >> rx;
+		sum = pref[rz][ry][rx]
+			- pref[rz][ry][lx - 1]
+			- pref[rz][ly - 1][rx]
+			- pref[lz - 1][ry][rx]
 
-                        - pref[lz - 1][ly - 1][rx]
-                        - pref[lz - 1][ry][lx - 1]
-                        - pref[rz][ly - 1][lx - 1]
+			+ pref[rz][ly - 1][lx - 1]
+			+ pref[lz - 1][ry][lx - 1]
+			+ pref[lz - 1][ly - 1][rx]
 
-                        + pref[lz - 1][ly - 1][lx - 1];
+			- pref[lz - 1][ly - 1][lx - 1];
 
-        cout << ans << ' ';
-    }
+		cout << sum << ' ';
+	}
 }

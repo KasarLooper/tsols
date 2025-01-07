8fa95f020727fa3c236e88666156b2fa53351385
Afanasyev Aleksander <kasar.looper@gmail.com>
2025-01-08 00:34:35 +0300

    Solved I from 1

--- 1_containers/i.cpp
+++ 1_containers/i.cpp
@@ -1,57 +1,56 @@
 #include <iostream>
-#include <deque>
 #include <stack>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-void fastIO() {
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-}
-
-void from_path(deque<int>& path, stack<int>& lock, int cur) {
-	int el = path.back();
-	path.pop_back();
-	while (!path.empty() && cur != el) {
-		lock.push(el);
-		el = path.back();
-		path.pop_back();
-	}
-}
-
-int from_lock(stack<int>& lock, int cur) {
-	while (!lock.empty() && lock.top() == cur) {
-		lock.pop();
-		cur++;
-	}
-	return cur;
-}
-
-int main() {
-	fastIO();
 
 	int n;
 	cin >> n;
 
-	deque<int> path;
-	stack<int> lock;
-
+	queue<int> path;
+	int x;
 	for (int i = 0; i < n; i++) {
-		int x;
 		cin >> x;
-		path.push_front(x);
-	}
-	
-	int cur = 1;
-	while (!path.empty()) {
-		from_path(path, lock, cur);
-		cur = from_lock(lock, cur);
+		path.push(x);
 	}
 
-	if (lock.empty()) {
-		cout << 1;
+	vector<pair<int, int>> res;
+	stack<int> dead;
+
+	int dest = 1;
+	while (!path.empty()) {
+		int moved = 0;
+		for (; (dead.empty() || dead.top() != dest) &&
+			!path.empty(); moved++) {
+			dead.push(path.front());
+			path.pop();
+		}
+		if (moved != 0) {
+			res.emplace_back(1, moved);
+		}
+
+		moved = 0;
+		for (; !dead.empty() && dead.top() == dest; moved++) {
+			dead.pop();
+			dest++;
+		}
+		if (moved != 0) {
+			res.emplace_back(2, moved);
+		}
+	}
+
+	if (dead.empty()) {
+		cout << res.size() << '\n';
+		for (auto el : res) {
+			cout << el.first << ' ' << el.second << '\n';
+		}
 	}
 	else {
-		cout << 0;
+		cout << "0\n";
 	}
 }

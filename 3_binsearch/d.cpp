65cb184cfe081cbe70ffbee4b11cf5a052931285
Afanasyev Aleksander <kasar.looper@gmail.com>
2025-01-08 15:12:11 +0300

    Solved D from 3

--- 3_binsearch/d.cpp
+++ 3_binsearch/d.cpp
@@ -1,21 +1,32 @@
-
 #include <iostream>
 
 using namespace std;
+typedef long long ll;
+
+bool check(ll n, ll a, ll b, ll w, ll h, ll guess) {
+	ll a1 = a + 2 * guess;
+	ll b1 = b + 2 * guess;
+	ll cnt1 = (w / a1) * (h / b1);
+	ll cnt2 = (w / b1) * (h / a1);
+	return max(cnt1, cnt2) >= n;
+}
 
 int main() {
-    long long n, a, b, w, h;
-    cin >> n >> a >> b >> w >> h;
-    long long l = 0, r = min(w, h);
-    while (l + 1 < r) {
-        long long m = l + (r - l) / 2;
-        if ((w / (a + m * 2)) * (h / (b + m * 2)) >= n) {
-            l = m;
-        }
-        else {
-            r = m;
-        }
-    }
-    if (l == 0) cout << 0 << endl;
-    else cout << l + (r - l + 1) / 2 << endl;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	ll n, a, b, w, h;
+	cin >> n >> a >> b >> w >> h;
+
+	ll l = 0, r = max(w, h);
+	ll m;
+	while (l + 1 < r) {
+		m = (l + r) / 2;
+		if (check(n, a, b, w, h, m))
+			l = m;
+		else
+			r = m;
+	}
+
+	cout << l << '\n';
 }

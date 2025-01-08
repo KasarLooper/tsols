9e086738e0640b97bdc225f249b4224eb544d55f
Afanasyev Aleksander <kasar.looper@gmail.com>
2025-01-08 19:43:14 +0300

    Solved D from 4

--- 4_stl/d.cpp
+++ 4_stl/d.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
-#include <set>
-#include <map>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
+typedef long long ll;
+
+struct Student {
+	string name, sirname, clas, birth;
+	
+	bool operator < (const Student& other) const {
+		if (clas != other.clas) {
+			int num = stoi(clas);
+			int other_num = stoi(other.clas);
+			if (num != other_num)
+				return num < other_num;
+			return clas < other.clas;
+		}
+		return sirname < other.sirname;
+	}
+};
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    map<string, set<string>> m;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        string sername, name, class_name, date;
-        cin >> sername >> name >> class_name >> date;
-        m[class_name].insert(sername + ' ' + name + ' ' + date);
-    }
+	int n;
+	cin >> n;
 
-    for (auto class_name : m)
-        for (auto student : class_name.second)
-            cout << class_name.first << ' ' << student << '\n';
+	vector<Student> students;
+	for (int i = 0; i < n; i++) {
+		Student cur;
+		cin >> cur.sirname >> cur.name 
+			>> cur.clas >> cur.birth;
+		students.push_back(cur);
+	}
+
+	sort(students.begin(), students.end());
+
+	for (auto el : students) {
+		cout << el.clas << ' ' << el.sirname << ' '
+			<< el.name << ' ' << el.birth << '\n';
+	}
 }
